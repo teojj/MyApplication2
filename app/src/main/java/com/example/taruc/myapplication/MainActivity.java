affa7130355b58e6960e92f286edad7cd366fc52
anreh <anurehka@gmail.com>
2018-10-17 13:18:08 +0800

    apa lanjiao!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/src/main/java/com/example/taruc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -33,9 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
